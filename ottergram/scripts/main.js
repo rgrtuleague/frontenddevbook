e11bb7ebf7ba9fc458725fb115fedd7acf27d126
sergey <rgrtuleague@gmail.com>
2018-01-09 11:08:06 +0300

    job146

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,3 +1,3 @@
-/**
- * Created by SEREGAVIKA on 08.01.2018.
- */
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
