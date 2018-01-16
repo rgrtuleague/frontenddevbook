bbfe4f55d560c824a40215c95a999b8d55b9f069
sergey <rgrtuleague@gmail.com>
2018-01-16 17:24:48 +0300

    job282 coffeerun

--- coffeerun/scripts/checklist.js
+++ coffeerun/scripts/checklist.js
@@ -2,7 +2,8 @@
     'use strict';
     var App = window.App || {};
     var $ = window.jQuery;
-    function CheckList(selector) {
+    function CheckList(selector)
+    {
         if (!selector) {
             throw new Error('No selector provided');
         }
@@ -11,6 +12,74 @@
             throw new Error('Could not find element with selector: ' + selector);
         }
     }
+
+    CheckList.prototype.addClickHandler = function (fn) {
+        this.$element.on('click', 'input', function (event) {
+            var email = event.target.value;
+            this.removeRow(email);
+            fn(email);
+        }.bind(this));
+    };
+
+    CheckList.prototype.addRow = function (coffeeOrder)
+    {
+        this.removeRow(coffeeOrder.emailAddress);
+        var rowElement = new Row(coffeeOrder);
+        this.$element.append(rowElement.$element);
+    };
+
+    CheckList.prototype.removeRow = function (email) {
+        this.$element
+            .find('[value="' + email + '"]')
+            .closest('[data-coffee-order="checkbox"]')
+            .remove();
+    };
+
+    function Row(coffeeOrder)
+    {
+        var $div = $('<div></div>', {
+            'data-coffee-order': 'checkbox', // Обратите внимание на одинарные кавычки. Они нужны, елси мы используем специальные символы: - (тире)
+            'class': 'checkbox' // Здесь class также берем в одинарные кавычки, потому что это зарезервированное JavaScript слово
+        });
+
+        var $label = $('<label></label>');
+
+        var $checkbox = $('<input></input>', {
+            type: 'checkbox',
+            value: coffeeOrder.emailAddress
+        });
+
+        var description = ' [' + coffeeOrder.strength + 'x] ';
+        description += coffeeOrder.size + ' ';
+        if (coffeeOrder.flavor) {
+            description += coffeeOrder.flavor + ' ';
+        }
+        description += coffeeOrder.coffee + ', ';
+        description += ' (' + coffeeOrder.emailAddress + ')';
+
+
+        $label.append($checkbox);
+        $label.append(description);
+        $div.append($label);
+
+
+
+        this.$element = $div;
+        switch (coffeeOrder.flavor)
+        {
+            case ('caramel'):
+                this.$element.css('color', 'brown');
+                break;
+            case ('almond'):
+                this.$element.css('color', 'blue');
+                break;
+            case ('mocha'):
+                this.$element.css('color', 'orange');
+                break;
+        }
+
+        }
+
     App.CheckList = CheckList;
     window.App = App;
 })(window);
