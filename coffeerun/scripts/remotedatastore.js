b6c31ffdf2ef3eeb5afdf90a47265e2c1ce2fb6a
sergey <rgrtuleague@gmail.com>
2018-01-19 14:53:41 +0300

    job316 coffeerun

--- coffeerun/scripts/remotedatastore.js
+++ coffeerun/scripts/remotedatastore.js
@@ -13,6 +13,39 @@
         this.serverUrl = url;
     }
 
+    RemoteDataStore.prototype.add = function (key, val)
+    {
+         $.post(this.serverUrl, val, function(serverResponse)
+         {
+             console.log(serverResponse);
+         });
+    };
+
+    RemoteDataStore.prototype.getAll = function (cb)
+    {
+        $.get(this.serverUrl, function (serverResponse)
+        {
+            console.log(serverResponse);
+            cb(serverResponse);
+        })
+    };
+
+    RemoteDataStore.prototype.get = function (key, cb)
+    {
+        $.get(this.serverUrl + '/' + key, function (serverResponse)
+        {
+            console.log(serverResponse);
+            cb(serverResponse);
+        })
+    };
+
+    RemoteDataStore.prototype.remove = function (key)
+    {
+        $.ajax(this.serverUrl + '/' + key, {
+            type: 'DELETE'
+        });
+    };
+
     App.RemoteDataStore = RemoteDataStore;
     window.App = App;
 })(window);
